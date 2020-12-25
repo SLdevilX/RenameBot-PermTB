b7915e83e23d3c4986b283ce61835693c125094a
SLdevilX <52373359+SLdevilX@users.noreply.github.com>
2020-12-25 19:46:04 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,27 +1,28 @@
 class Translation(object):
-    START_TEXT = """ A Simple File Renamer Bot With Permanent Thumbnail support!💯
+    START_TEXT = """ A Simple File Renamer Bot from **Danuma projects** 🤫 
 
 <b>Send me any Telegram file and reply to that file with /rename NewName.extn</b>"""
 
-    RENAME_403_ERR = "What Are You Doing? You are Banned"
-    ABS_TEXT = "What Are You Trying To Do,Mate?"
-    UPGRADE_TEXT = "CONTACT @prgofficial"
-    DOWNLOAD_START = "Give Me Some Time Bruh!!(〒﹏〒)"
-    UPLOAD_START = "Hurray... Completed Downloading(;ŏ﹏ŏ)"
+    RENAME_403_ERR = "තමුසෙ මොනවද ඔය කරන්නෙ තමුසෙව බෑන් කරා😤"
+    ABS_TEXT = "විකාරනේ කරන්නේ😒?"
+    UPGRADE_TEXT = "එනෝ ගෲප් එකට  @danuma01"
+    DOWNLOAD_START = "චුට්ටක් ඉන්නෝ මේ ඩව්න් කරන ගමන්😁"
+    UPLOAD_START = "හරේ මේ අප්ලෝඩ් කරනෝ😉"
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry WTF Do You Think! I'll Upload It?"
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > ©  @prgofficial **"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**This bot from officially © @danuma01 **"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds"
-    NOT_AUTH_USER_TEXT = "CONTACT @prgofficial"
-    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @prgofficial"
-    SAVED_CUSTOM_THUMB_NAIL = "Thumbnail Saved ✅ This Is Permanent"
-    DEL_ETED_CUSTOM_THUMB_NAIL = "Custom thumbnail cleared succesfully🤦"
-    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
-    SAVED_RECVD_DOC_FILE = "File Downloaded Successfully 😎"
+    NOT_AUTH_USER_TEXT = "CONTACT @danuma01"
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @danuma01"
+    SAVED_CUSTOM_THUMB_NAIL = "හරී ඔන්න තම්නල් එක සේව් කරා permanently ✔️ "
+    DEL_ETED_CUSTOM_THUMB_NAIL = "තම්නල් එක සාර්තකව මකලා දැම්මා🤦"
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "සේරම මකලා දැම්මා."
+    SAVED_RECVD_DOC_FILE = "ඔන්න ඩවුන්ලෝඩ්කරා 😎"
     CUSTOM_CAPTION_UL_FILE = " "
-    NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found,SED🤒"
+    NO_CUSTOM_THUMB_NAIL_FOUND = "කෝ තම්නල් එකක් හම්බුනේ නෑනි🤒"
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     HELP_USER = """It's not that complicated😅
     
-1. Send me any Telegram File.
-2. Reply to that message with /rename NEWNAME.EXTENSION"""
+1. මුලිං මට ෆයිල් එකක් එවන්න.
+2. ඊටපස්සේ  /rename NEWNAME.EXTENSION නිව් නේම් එකට අලුත් නම දාල
+එක්ස්ටෙන්ශන් එක එක්ක රිප්ල්‍යි එකක් දාන්න"""
 
